776c33433bb4f34f8e361cc967d9f0623c6a851c
Contreras Perales Rodrigo <levicom134@gmail.com>
2024-09-23 12:17:59 -0500

    Create done in react

---
+++ django_crud_api/client/src/components/TaskCard.jsx
@@ -0,0 +1,9 @@
+export function TaskCard({ task }){
+    return(
+        <div key={task.id}>
+                <h1>{task.title}</h1>
+                <p>{task.description}</p>
+                <hr />
+        </div>
+    )
+}
--- django_crud_api/client/src/pages/TasksFormPage.jsx
+++ django_crud_api/client/src/pages/TasksFormPage.jsx
@@ -1,5 +1,36 @@
+import {useForm} from 'react-hook-form';
+import { createTask } from '../api/tasks.api';
+import {useNavigate} from 'react-router-dom';
+
 export function TaskFormPage(){
+    const {register,handleSubmit, formState:{
+        errors
+    }} = useForm();
+
+    const onSubmit = handleSubmit((data)=>{
+        console.log(data);
+        createTask(data).then((response)=>{
+            console.log(response);
+        }).catch((error)=>{
+            console.log(error);
+        })
+    });
     return(
-        <div>Task Form Page</div>
+        <div>
+            <form onSubmit={onSubmit}>
+                <input type="text" 
+                placeholder="Title" 
+                {...register("title",{required:true})}
+                />
+                {errors.title && <span>Title is required</span>}
+                <textarea
+                
+                 placeholder="Description"
+                {...register("description",{required:true})}
+                ></textarea>
+                {errors.description && <span>Description is required</span>}
+                <button type="submit">Create Task</button>
+            </form>
+        </div>
     )
 }
