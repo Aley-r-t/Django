90c924e8f85dfb46350c965f1c6a0e1c1a3db1fb
Contreras Perales Rodrigo <levicom134@gmail.com>
2024-09-23 11:03:40 -0500

    Create rutas react

--- django_crud_api/client/src/App.jsx
+++ django_crud_api/client/src/App.jsx
@@ -1,13 +1,16 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import {TasksPage} from './pages/TasksPage'
 import { TaskFormPage } from './pages/TasksFormPage'
+import { Navigation } from './components/Navigation'
 
 function App(){
   return(
     <BrowserRouter>
+    <Navigation />
     <Routes>
+      <Route path='/' element={<Navigate to="/tasks" />} />
       <Route path="/tasks" element={<TasksPage />} />
-      <Route path="/tasks/new" element={<TaskFormPage />} />
+      <Route path="/tasks-create" element={<TaskFormPage />} />
     </Routes>
     </BrowserRouter>
   )
---
+++ django_crud_api/client/src/components/Navigation.jsx
@@ -0,0 +1,11 @@
+import {Link} from 'react-router-dom';
+
+export function Navigation(){
+    return(
+        <div>
+            <Link to="/tasks">Task App</Link><br/>
+            <Link to="/tasks-create">Create Task</Link>
+        </div>
+    )
+}
+
