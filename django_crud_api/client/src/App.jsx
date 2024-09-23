380af5b5dc3070de91e862961c26247b55edffea
Contreras Perales Rodrigo <levicom134@gmail.com>
2024-09-23 10:46:19 -0500

    Api en Django create

---
+++ django_crud_api/client/src/App.jsx
@@ -0,0 +1,16 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {TasksPage} from './pages/TasksPage'
+import { TaskFormPage } from './pages/TasksFormPage'
+
+function App(){
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route path="/tasks" element={<TasksPage />} />
+      <Route path="/tasks/new" element={<TaskFormPage />} />
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
