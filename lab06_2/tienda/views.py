4baf72a53d141f358d018bb33c06ffac7868ba70
Contreras Perales Rodrigo <levicom134@gmail.com>
2024-09-28 09:14:18 -0500

    Tarea06_2 1 done

--- lab06_2/tienda/views.py
+++ lab06_2/tienda/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Producto
+from .models import Producto, Categoria
 
 # Create your views here.
+
+
+
 def index(request):
     product_list = Producto.objects.order_by('nombre')[:6]
-    context = {'product_list': product_list}
+    categoria_list = Categoria.objects.values('nombre').distinct()
+    context = {'product_list': product_list, 'categoria_list': categoria_list}
     return render(request, 'tienda/index.html', context)
 
 def producto(request, producto_id):
     producto = get_object_or_404(Producto, pk=producto_id)
-    return render(request, 'tienda/producto.html',{ 'producto': producto })
+    categoria_list = Categoria.objects.values('nombre').distinct()
+    context = {'producto': producto, 'categoria_list': categoria_list}
+    return render(request, 'tienda/producto.html',context)
